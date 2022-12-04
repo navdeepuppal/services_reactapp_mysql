f8457b5083630261d57460d1fcd2a54e19f73c22
navdeepuppal <navdeepuppal1609@gmail.com>
2022-12-04 23:33:06 +0530

    first commit

--- components/SubSubServicesHeader.js
+++ components/SubSubServicesHeader.js
@@ -1,7 +1,6 @@
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { COLORS, FONTS, SIZES, assets } from "../constants";
-import { ImageSlider } from "react-native-image-slider-banner";
 
 
 const SubSubServicesHeader = ({ onSearch, SubS_Name }) => {
@@ -51,16 +50,7 @@ const SubSubServicesHeader = ({ onSearch, SubS_Name }) => {
 				</TouchableOpacity>
 			</View>
 
-			<ImageSlider 
-    data={[
-        {img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5a5uCP-n4teeW2SApcIqUrcQApev8ZVCJkA&usqp=CAU'},
-        {img: 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg'},
-        {img: 'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg'}
-    ]}
-    autoPlay={false}
-    onItemChanged={(item) => console.log("item", item)}
-    closeIconColor="#fff"
-/>
+			
 
 
 
